35eb0b234ea6d86c3806810806db6fa94b4f14fa
lbogacz <lukas.bogacz@gmail.com>
2019-07-17 23:43:36 +0100

    navbar, footer home-page have been changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,12 @@
 <template>
   <footer>
-    <v-footer class="pa-4">
-      <div>
+    <v-footer height="400">
+      <div class="footer">
         <p>Design by lbogacz &copy;{{new Date().getFullYear()}}</p>
       </div>
+      <!-- <div class="img">
+        <v-img :src="require('../assets/hse-3464155_1920.jpg')"></v-img>
+      </div>-->
     </v-footer>
   </footer>
 </template>
@@ -18,5 +21,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// .img{
+//   width: 400px;
+//   height: 400px;
+// }
+.v-footer {
+  background-image: url(..//assets/hse-3464155_1920.jpg);
+  background-position:center;
+  background-size:cover;
+  background-repeat: no-repeat;
 
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  color: #fff;
+}
 </style>
