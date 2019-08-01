eef4b54d4ce54e8b9b2042949cf1fdc01536452c
lbogacz <lukas.bogacz@gmail.com>
2019-08-02 00:10:33 +0100

    text has benn added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,9 +12,14 @@
         <img v-for="picture in pictures" :key="picture.img " :src="picture.img" />
       </div>
     </div>
-    <div class="home-header">
-      <h2>Lorem ipsum dolor, sit.</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores officiis quas ea quis vero quisquam reprehenderit dolorem consequatur similique architecto, beatae numquam, obcaecati quaerat ipsum excepturi eius doloremque aliquam laudantium!</p>
+    <div class="about-company">
+      <h2>About company</h2>
+      <img src="../assets/heather-ford-1270604-unsplash.jpg" />
+
+      <p>
+        We are a professional cleaning company with experience. We offer comprehensive cleaning of apartments and offices at favorable prices. We guarantee the highest standard of cleaning and additional services.
+        You can often find the opinion that it is not easy to find a good and at the same time inexpensive cleaner or - in the case of companies and institutions - cleaning company. The fact is that there are many entities providing similar services on the market as we do, but not always clients of these companies are satisfied with their work. However, it is not worth spreading this unfavorable opinion to all cleaning companies, because on the other hand there are companies such as ours, for whom the highest quality of services and the satisfaction of each client are a priority. We believe that this is the only way to build a company's reputation and encourage new customers to cooperate with us. Words unsupported by deeds do not mean much - that's why, as cleaning specialists, we engage 100% in every job. The cleaning lady from our cleaning company guarantees a perfect work, with attention to every detail and property entrusted to us. It's not without reason that our services are so eagerly recommended by our existing customers.
+      </p>
     </div>
   </div>
 </template>
@@ -61,6 +66,11 @@ export default {
   }
 }
 
+.about-company {
+  img {
+    display: none;
+  }
+}
 @media (min-width: 500px) {
   .home-header {
     font-size: 15px;
@@ -106,8 +116,7 @@ export default {
 
 @media (min-width: 950px) {
   .home-header {
-    h1,
-    h2 {
+    h1 {
       font-size: 200%;
       text-align: center;
       margin: auto;
@@ -132,9 +141,133 @@ export default {
     max-height: 100vh;
   }
   img {
+    max-height: 100vh;
     max-width: 25vw;
     padding: 10px;
   }
+  .about-company {
+    max-width: 100%;
+    background-color: rgb(196, 179, 179);
+    margin-top: 20px;
+
+    h2 {
+      font-size: 200%;
+      padding: 30px;
+    }
+
+    h2,
+    p {
+      text-align: center;
+      margin: auto;
+    }
+    p {
+      max-width: 80%;
+      font-size: 140%;
+      padding: 15px;
+    }
+  }
+}
+
+@media (min-width: 960px) {
+  .about-company {
+    h2 {
+      margin-left: 20%;
+    }
+    img {
+      display: inline-block;
+      position: relative;
+      left: 0;
+      top: -100px;
+      max-height: 100vh;
+      max-width: 25vw;
+    }
+    p {
+      font-size: 140%;
+      display: inline-block;
+      max-width: 70%;
+      position: relative;
+      left: 0;
+      top: -20px;
+    }
+  }
+}
+
+@media (min-width: 1040px) {
+  .about-company {
+    margin-top: 20px;
+    padding: 30px;
+
+    h2 {
+      margin-left: 40%;
+      font-size: 200%;
+      padding: 30px;
+    }
+    img {
+      display: inline-block;
+      position: relative;
+      top: -20px;
+      left: 0;
+      max-height: 50vh;
+      max-width: 25vw;
+    }
+    p {
+      font-size: 140%;
+      display: inline-block;
+      max-width: 65%;
+      position: relative;
+      left: 10%;
+    }
+  }
+}
+
+@media (min-width: 1400px) {
+  .home-header,
+  .about-company {
+    line-height: 30px;
+  }
+  .about-company {
+    h2 {
+      margin-left: 20%;
+      font-size: 200%;
+      padding: 30px;
+    }
+    img {
+      left: 8%;
+      max-height: 50vh;
+      max-width: 25vw;
+    }
+    p {
+      max-width: 60%;
+      position: relative;
+      top: -60px;
+    }
+  }
+}
+@media (min-width: 1600px) {
+  .home-header,
+  .about-company {
+    line-height: 40px;
+  }
+  .about-company {
+    p {
+      top: -40px;
+    }
+  }
+}
+
+@media(min-width:1900px){
+  .about-company{
+    p{
+      top:-80px;
+    }
+  }
+}
+
+@media(min-width:2060px){
+  .about-company{
+    p{
+      top:-100px;
+    }
+  }
 }
 </style>
-
